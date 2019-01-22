3fefe174e1ce907aeada1edf0ed2092047f00999
Sebastian Perek <sperek@ptc.com>
2019-01-22 14:43:32 +0100

    initial commit

---
+++ src/test/java/pl/com/psc/tt/hexample/HexampleApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.com.psc.tt.hexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HexampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
